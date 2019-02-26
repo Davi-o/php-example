785449e9562c53edcd8b383fcf04547dba1011c0
Davi Alves <alvesdelima.davi@gmail.com>
2019-02-26 12:50:32 -0300

    Validation with error

--- util/validation.php
+++ util/validation.php
@@ -1,15 +1,18 @@
 <?php
 class Validation{
-    public static function crossSiteScripting($exp,$v){
+    public static function crossSiteScripting($v){
         return htmlspecialchars(trim($v));
     }
-    public static function validateName($exp,$v){
+    public static function validateName($v){
         $exp = "/^[a-zA-Z ]{2,20}$/";
+        return trim(preg_match($exp,$v));
     }
-    public static function validateSurname($exp,$v){
+    public static function validateSurname($v){
         $exp = "/^[a-zA-Z ]{2,50}$/";
+        return trim(preg_match($exp,$v));
     }
-    public static function validadeBirthDate($exp,$v){
+    public static function validadeBirthDate($v){
         $exp = "/^[a-zA-Z 0-9]{}$/";
+        return trim(preg_match($exp,$v));
     }
 }
